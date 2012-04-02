0424ba915738df9d4b79b3e7859bd14de5255631
Scott J. Krieder <skrieder@iit.edu>
2012-04-02 10:47:51 -0500

    changing c sleep to use args

--- c/sleep.c
+++ c/sleep.c
@@ -1,5 +1,10 @@
-int main()
+#include <stdio.h>
+
+int main(int argc, char *argv[])
 {
+  if(argc < 2){
+    printf("Sleep n, where n is a number in seconds to sleep.");
+  }
   sleep(0); //Parameter in sleep is in seconds
   return 0;
 }
